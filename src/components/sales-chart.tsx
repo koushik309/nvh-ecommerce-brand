ab78deb3ca011d151f47e9cbc7b47cb306d1870d
NZAR <98880087+heynzar@users.noreply.github.com>
2024-12-31 23:57:29 +0100

    📌fix: sales chart data

--- src/components/sales-chart.tsx
+++ src/components/sales-chart.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
-import { salesChartData } from "@/lib/app-data";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   ChartConfig,
@@ -9,6 +8,37 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+
+const salesChartData = [
+  { day: 1, sales: 2100, profits: 525 },
+  { day: 2, sales: 2200, profits: 550 },
+  { day: 3, sales: 2200, profits: 500 },
+  { day: 4, sales: 2200, profits: 575 },
+  { day: 5, sales: 2200, profits: 625 },
+  { day: 6, sales: 2200, profits: 600 },
+  { day: 7, sales: 2200, profits: 650 },
+  { day: 8, sales: 2500, profits: 625 },
+  { day: 9, sales: 2700, profits: 675 },
+  { day: 10, sales: 2800, profits: 700 },
+  { day: 11, sales: 2600, profits: 650 },
+  { day: 12, sales: 2900, profits: 725 },
+  { day: 13, sales: 2700, profits: 675 },
+  { day: 14, sales: 1800, profits: 430 },
+  { day: 15, sales: 1700, profits: 400 },
+  { day: 16, sales: 1600, profits: 385 },
+  { day: 17, sales: 2000, profits: 550 },
+  { day: 18, sales: 3200, profits: 850 },
+  { day: 19, sales: 3200, profits: 800 },
+  { day: 20, sales: 3200, profits: 875 },
+  { day: 21, sales: 3200, profits: 825 },
+  { day: 22, sales: 3600, profits: 900 },
+  { day: 23, sales: 3400, profits: 850 },
+  { day: 24, sales: 3700, profits: 925 },
+  { day: 25, sales: 3900, profits: 975 },
+  { day: 26, sales: 3800, profits: 950 },
+  { day: 27, sales: 4000, profits: 1000 },
+  { day: 28, sales: 4200, profits: 1050 },
+];
 
 const chartConfig = {
   sales: {
