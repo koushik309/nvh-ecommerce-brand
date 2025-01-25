03ebda2ba9c5e8003bd7e82cd73e553c0b9256e9
NZAR <98880087+heynzar@users.noreply.github.com>
2025-01-25 10:22:14 +0100

    feat: add hero section

---
+++ src/components/store/store-hero.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import hero from "../../../public/hero.png";
+
+export default function HeroSection() {
+  return (
+    <section className="select-none my-24 flex justify-start items-center border  w-full h-[200px] relative max-w-screen-lg mx-auto rounded-t-xl">
+      <Image
+        src={hero}
+        alt="cover"
+        className="absolute bottom-0 right-0 h-[300px] w-auto"
+      />
+      <p className="ml-2 text-6xl leading-[62px] font-extralight italic">
+        Timeless Beauty, Redefined. <br />
+        Your Skin Deserves{" "}
+        <span className="underline underline-offset-8 font-medium">
+          the Best.
+        </span>
+      </p>
+    </section>
+  );
+}
