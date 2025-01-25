e8f879a9a36f3a5f08ed0a195cfea1419eb82de4
NZAR <98880087+heynzar@users.noreply.github.com>
2025-01-25 08:02:30 +0100

    feat: add header section

---
+++ src/components/store/store-header.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { CardButton } from "./store-cart-button";
+
+export default function Header() {
+  return (
+    <header>
+      <header className="py-10 px-4 flex flex-col gap-10">
+        <h1 className="text-center text-7xl font-bold uppercase">
+          NZAR Velvet Hour
+        </h1>
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <h2 className="font-medium text-2xl">NVH®</h2>
+          </Link>
+          <nav>
+            <ul className="flex gap-10">
+              <li className="hover:underline underline-offset-8 cursor-pointer">
+                <a href="#our-collection">Our Collection</a>
+              </li>
+              <li className="hover:underline underline-offset-8 cursor-pointer">
+                <a href="#client-favorites">Client Favorites</a>
+              </li>
+              <li className="hover:underline underline-offset-8 cursor-pointer">
+                <a href="#why-nvh">Why NVH®</a>
+              </li>
+              <li className="hover:underline underline-offset-8 cursor-pointer">
+                <a href="#about-the-brand">About The Brand</a>
+              </li>
+            </ul>
+          </nav>
+
+          <CardButton />
+        </div>
+      </header>
+    </header>
+  );
+}
