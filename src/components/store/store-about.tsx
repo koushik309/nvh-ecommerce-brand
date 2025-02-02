75bda37ab67278a101231a07ea77504036a3dabd
NZAR <98880087+heynzar@users.noreply.github.com>
2025-02-02 12:21:55 +0100

    feat: implement responsive About, BestSeller sections

--- src/components/store/store-about.tsx
+++ src/components/store/store-about.tsx
@@ -4,29 +4,32 @@ import { Star, StarHalf } from "lucide-react";
 
 export default function About() {
   return (
-    <section id="about-the-brand" className="py-24 max-w-screen-lg mx-auto">
-      <div className="text-center mb-16">
-        <h2 className="text-5xl mb-3 font-light">About The Brand</h2>
-        <p className="text-lg text-neutral-400">
+    <section
+      id="about-the-brand"
+      className="py-12 sm:py-24 max-w-screen-lg mx-auto px-4"
+    >
+      <div className="text-center mb-10 sm:mb-16">
+        <h2 className="text-3xl sm:text-5xl font-light text-white mb-3">
+          About The Brand
+        </h2>
+        <p className="sm:text-lg text-white/60">
           Redefining Luxury Skincare Since 2021
         </p>
       </div>
 
       <div className="w-full gap-6 flex flex-col items-center justify-center py-12 px-8 bg-[#F8F8F8] rounded-2xl shadow-sm">
-        <Image
-          src={nvh}
-          alt="NVH Logo"
-          className="h-10 w-auto select-none mb-2"
-        />
+        <Image src={nvh} alt="NVH Logo" className="h-10 w-auto mb-2" />
         <p className="text-center text-lg text-neutral-800 max-w-[600px] leading-relaxed">
           Born from the fusion of nature&apos;s finest ingredients and
           cutting-edge science, NVH® represents the pinnacle of luxury skincare.
-          Each formula is meticulously crafted to deliver transformative
-          results.
+          <span className="hidden sm:inline-block">
+            Each formula is meticulously crafted to deliver transformative
+            results.
+          </span>
         </p>
 
-        <div className="flex gap-8 w-full py-8 justify-center">
-          <div className="h-[80px] w-[32%] text-neutral-800 justify-center items-center rounded-xl flex gap-3 bg-white p-4 shadow-sm">
+        <div className="flex flex-col md:flex-row md:flex-[1_0_32%] gap-4 md:gap-8 w-full py-6 justify-center">
+          <div className="h-[80px] text-neutral-800 justify-center items-center rounded-xl flex gap-3 bg-white p-4 shadow-sm">
             <h3 className="text-5xl font-light tracking-tight">94%</h3>
             <div className="flex flex-col items-start">
               <span className="text-xs font-medium tracking-wider text-neutral-500">
@@ -35,7 +38,7 @@ export default function About() {
               <span className="text-sm font-medium">Enhanced Hydration*</span>
             </div>
           </div>
-          <div className="h-[80px] w-[32%] text-neutral-800 justify-center items-center rounded-xl flex gap-3 bg-white p-4 shadow-sm">
+          <div className="h-[80px] text-neutral-800 justify-center items-center rounded-xl flex gap-3 bg-white p-4 shadow-sm">
             <h3 className="text-5xl font-light tracking-tight">85%</h3>
             <div className="flex flex-col items-start">
               <span className="text-xs font-medium tracking-wider text-neutral-500">
@@ -44,7 +47,7 @@ export default function About() {
               <span className="text-sm font-medium">Increased Radiance**</span>
             </div>
           </div>
-          <div className="h-[80px] w-[32%] text-neutral-800 justify-center items-center rounded-xl flex gap-3 bg-white p-4 shadow-sm">
+          <div className="h-[80px] text-neutral-800 justify-center items-center rounded-xl flex gap-3 bg-white p-4 shadow-sm">
             <h3 className="text-5xl font-light tracking-tight">88%</h3>
             <div className="flex flex-col items-start">
               <span className="text-xs font-medium tracking-wider text-neutral-500">
@@ -55,8 +58,10 @@ export default function About() {
           </div>
         </div>
 
-        <p className="text-sm text-neutral-600 font-medium flex   gap-2">
-          <span>Trusted by over 3,218 customers with an average rating of</span>
+        <p className="text-sm text-neutral-600 font-medium flex flex-col md:flex-row justify-center items-center  gap-2">
+          <span className="text-center">
+            Trusted by over 3,218 customers with an average rating of
+          </span>
           <span>
             <span className="inline-flex justify-center gap-1">
               <Star className="size-4 fill-yellow-400 text-yellow-400" />
--- src/components/store/store-best-seller.tsx
+++ src/components/store/store-best-seller.tsx
@@ -5,37 +5,36 @@ export default function BestSeller() {
   return (
     <section
       id="client-favorites"
-      className="py-24 max-w-screen-lg mx-auto select-none"
+      className="py-12 sm:py-24 max-w-screen-lg mx-auto px-4"
     >
       <div className="text-center mb-16">
-        <h2 className="text-5xl mb-3 font-light">Client Favorites</h2>
-        <p className="text-lg text-neutral-400">
+        <h2 className="text-3xl sm:text-5xl font-light text-white mb-3">
+          Client Favorites
+        </h2>
+        <p className="sm:text-lg text-white/60">
           Our signature formulations that have earned global recognition
         </p>
       </div>
-      <div className="w-full relative flex flex-row gap-10 items-center justify-center h-[280px] bg-[#F8F8F8] rounded-xl">
-        <span className="absolute -right-12 top-6 text-lg bg-black rotate-45 py-1.5 px-20">
-          139$
-        </span>
+      <div className="w-full relative flex flex-col md:flex-row gap-10 items-center justify-center md:h-64 bg-[#F8F8F8] rounded-xl p-2">
         <Image
           src="/best.png"
           alt="Eternal Youth Serum"
           width={600}
           height={763}
-          className="absolute left-28 bottom-4 h-[330px] w-auto"
+          className="absolute left-1/5 md:left-10 lg:left-16 -top-12 md:-top-16 h-60 md:h-80 w-auto"
         />
 
-        <div className="ml-48">
-          <h3 className="text-5xl font-semibold text-black mb-2">
+        <div className="mt-52 md:mt-0 md:ml-48 flex flex-col justify-center text-center md:text-start md:justify-start">
+          <h3 className="text-3xl md:text-5xl font-semibold text-black mb-2">
             Eternal Youth
           </h3>
-          <p className="text-black text-lg mb-6 max-w-[380px]">
+          <p className="text-black leading-6 text-lg mb-6 max-w-[380px]">
             A luxury serum enriched with vitamins C and E to restore your
             skin&apos;s natural glow.
           </p>
-          <button className="py-2.5 flex items-center gap-2 hover:bg-black/80 transition-colors duration-300 px-8 bg-black rounded-lg font-medium">
+          <button className="py-2.5 flex items-center justify-center gap-2 hover:bg-black/80 transition-colors duration-300 px-8 bg-black rounded-lg font-medium">
             <ShoppingCart className="mr-2 h-5 w-5" />
-            Add to Cart
+            Add to Cart - 139$
           </button>
         </div>
       </div>
