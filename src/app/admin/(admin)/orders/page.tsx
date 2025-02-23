0ed7fd49b2b18959c681df644b013173b9b2ed61
heynzar <heynzar@gmail.com>
2025-02-23 07:13:56 +0000

    add mock data for orders

--- src/app/admin/(admin)/orders/page.tsx
+++ src/app/admin/(admin)/orders/page.tsx
@@ -1,7 +1,7 @@
 import { DataTable } from "@/components/dashboard-orders/data-table";
 import { columns } from "@/components/dashboard-orders/columns";
-import { order } from "@/lib/orders";
+import { orders } from "@/lib/orders";
 
 export default function Orders() {
-  return <DataTable data={order} columns={columns} />;
+  return <DataTable data={orders} columns={columns} />;
 }
